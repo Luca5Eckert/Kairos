1f887a3637234d7abe438900b0ffaed06bbc659c
Luca5Eckert <ecklukatv@gmail.com>
2026-04-05 01:23:02 -0300

    #2 refactor: change index to hnsw

--- infra/init/postgres/02_schema.sql
+++ infra/init/postgres/02_schema.sql
@@ -2,8 +2,9 @@ CREATE EXTENSION IF NOT EXISTS vector;
 
 CREATE TABLE sources (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    title TEXT NOT NULL,
     content TEXT NOT NULL,
     embedding vector(384) NOT NULL
 );
 
-CREATE INDEX ON sources USING ivfflat (embedding vector_cosine_ops);
+CREATE INDEX ON sources USING hnsw (embedding vector_cosine_ops);
