0e8e240400baa5fa119d72e99cd34c7485c16782
Luca5Eckert <lucasismaeleckert@gmail.com>
2026-04-06 18:07:44 -0300

    refactor: remove unused import in SourceEventPublisher interface

--- src/main/java/com/kairos/domain/event/SourceEventPublisher.java
+++ src/main/java/com/kairos/domain/event/SourceEventPublisher.java
@@ -1,7 +1,5 @@
 package com.kairos.domain.event;
 
-import com.kairos.domain.model.Source;
-
 public interface SourceEventPublisher {
 
     void send(CreatedSourceEvent event);
--- src/main/java/com/kairos/infrastructure/persistence/repository/concept/Neo4jConceptRepository.java
+++
@@ -1,9 +0,0 @@
-package com.kairos.infrastructure.persistence.repository.concept;
-
-import com.kairos.infrastructure.persistence.entity.ConceptNode;
-import org.springframework.data.neo4j.repository.Neo4jRepository;
-
-import java.util.UUID;
-
-public interface Neo4jConceptRepository extends Neo4jRepository<ConceptNode, UUID> {
-}
