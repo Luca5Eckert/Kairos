6bc95592f00d482431524403daf747e79d5c3f52
Luca5Eckert <ecklukatv@gmail.com>
2026-04-07 01:48:18 -0300

    fix: change log level from warn to debug for empty triples in KnowledgeGraphStoreAdapter

--- src/main/java/com/kairos/KairosApplication.java
+++ src/main/java/com/kairos/KairosApplication.java
@@ -2,8 +2,10 @@ package com.kairos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class KairosApplication {
 
 	public static void main(String[] args) {
