0b30c5abbbfef7244d73363701fe52b2e1dfe041
Luca5Eckert <ecklukatv@gmail.com>
2026-04-07 01:47:54 -0300

    #8 fix: add asynchronous handling for CreatedSourceEvent in CreatedSourceListener

--- src/main/java/com/kairos/infrastructure/event/consumer/CreatedSourceListener.java
+++ src/main/java/com/kairos/infrastructure/event/consumer/CreatedSourceListener.java
@@ -4,6 +4,7 @@ import com.kairos.application.command.GenerateSourceContextCommand;
 import com.kairos.application.use_case.GenerateSourceContextUseCase;
 import com.kairos.domain.event.CreatedSourceEvent;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionalEventListener;
 
@@ -17,6 +18,7 @@ public class CreatedSourceListener {
         this.generateSourceContextUseCase = generateSourceContextUseCase;
     }
 
+    @Async
     @TransactionalEventListener(CreatedSourceEvent.class)
     public void handleCreatedSourceEvent(CreatedSourceEvent event) {
         log.info("Received CreatedSourceEvent for sourceId: {}", event.sourceId());
