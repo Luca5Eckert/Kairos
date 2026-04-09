441a1fdb00b48259acdbf1d2111041f0b37f9b5a
Luca5Eckert <lucasismaeleckert@gmail.com>
2026-04-09 18:07:36 -0300

    #22 feat: update UploadSourceRequest to include title, content, and authorId fields

--- src/main/java/com/kairos/presentation/dto/request/UploadSourceRequest.java
+++ src/main/java/com/kairos/presentation/dto/request/UploadSourceRequest.java
@@ -2,7 +2,11 @@ package com.kairos.presentation.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
 
+import java.util.UUID;
+
 public record UploadSourceRequest(
-        @NotBlank String text
+        @NotBlank String title,
+        @NotBlank String content,
+        @NotBlank UUID authorId
 ) {
 }
