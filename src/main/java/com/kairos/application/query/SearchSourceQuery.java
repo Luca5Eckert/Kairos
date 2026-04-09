48a0799c19d2fa874e71b315ac6414349e180536
Luca5Eckert <lucasismaeleckert@gmail.com>
2026-04-09 18:11:06 -0300

    #22 feat: implement search context retrieval in SourceController

--- src/main/java/com/kairos/application/query/SearchSourceQuery.java
+++ src/main/java/com/kairos/application/query/SearchSourceQuery.java
@@ -3,4 +3,7 @@ package com.kairos.application.query;
 public record SearchSourceQuery(
         String searchTerm
 ) {
+    public static SearchSourceQuery of(String searchTerm) {
+        return new SearchSourceQuery(searchTerm);
+    }
 }
--- src/main/java/com/kairos/presentation/controller/SourceController.java
+++ src/main/java/com/kairos/presentation/controller/SourceController.java
@@ -1,27 +1,28 @@
 package com.kairos.presentation.controller;
 
 import com.kairos.application.command.UploadSourceCommand;
+import com.kairos.application.query.SearchSourceQuery;
 import com.kairos.application.use_case.GenerateSourceContextUseCase;
+import com.kairos.application.use_case.SearchSourceUseCase;
 import com.kairos.application.use_case.UploadSourceUseCase;
+import com.kairos.domain.model.SearchResult;
+import com.kairos.presentation.dto.request.GenerateSourceContextRequest;
 import com.kairos.presentation.dto.request.UploadSourceRequest;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/sources")
 public class SourceController {
 
     private final UploadSourceUseCase uploadSourceUseCase;
-    private final GenerateSourceContextUseCase generateSourceContextUseCase;
+    private final SearchSourceUseCase searchSourceUseCase;
 
-    public SourceController(UploadSourceUseCase uploadSourceUseCase, GenerateSourceContextUseCase generateSourceContextUseCase) {
+    public SourceController(UploadSourceUseCase uploadSourceUseCase, SearchSourceUseCase searchSourceUseCase) {
         this.uploadSourceUseCase = uploadSourceUseCase;
-        this.generateSourceContextUseCase = generateSourceContextUseCase;
+        this.searchSourceUseCase = searchSourceUseCase;
     }
 
     @PostMapping
@@ -38,6 +39,16 @@ public class SourceController {
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .build();
+    }
+
+    @GetMapping
+    public ResponseEntity<SearchResult> generateSourceContext(
+            @RequestBody @Valid GenerateSourceContextRequest request
+    ) {
+        var query = SearchSourceQuery.of(request.termQuery());
+        var context = searchSourceUseCase.execute(query);
+
+        return ResponseEntity.ok(context);
     }
 
 
