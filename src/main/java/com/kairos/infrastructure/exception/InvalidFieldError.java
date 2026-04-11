3ccc51a7f2aec3b4c7c3301c88ce837a8693ef88
Luca5Eckert <lucasismaeleckert@gmail.com>
2026-04-10 21:47:52 -0300

    #21 feat: add GlobalHandlerException

---
+++ src/main/java/com/kairos/infrastructure/exception/InvalidFieldError.java
@@ -0,0 +1,12 @@
+package com.kairos.infrastructure.exception;
+
+public record InvalidFieldError(
+        String field,
+        String error
+) {
+
+    public InvalidFieldError of(String field, String error){
+        return new InvalidFieldError(field, error);
+    }
+
+}
