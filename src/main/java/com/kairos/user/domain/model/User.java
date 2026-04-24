f182e6aaca545ee8ac36795caf0c7ca718ee4de8
luca5eckert <lucasismaeleckert@gmail.com>
2026-04-24 16:58:17 -0300

    #31 feat: add User model

---
+++ src/main/java/com/kairos/user/domain/model/User.java
@@ -0,0 +1,72 @@
+package com.kairos.user.domain.model;
+
+public class User {
+
+    private final Long id;
+
+    private String name;
+
+    private String username;
+
+    private String email;
+
+    private String hashPassword;
+
+    private Role role;
+
+    protected User(Long id, String name, String username, String email, String hashPassword) {
+        this.id = id;
+        this.name = name;
+        this.username = username;
+        this.email = email;
+        this.hashPassword = hashPassword;
+    }
+
+    public User(Builder builder) {
+        this.id = builder.id;
+        this.name = builder.name;
+        this.username = builder.username;
+        this.email = builder.email;
+        this.hashPassword = builder.hashPassword;
+    }
+
+
+    public static class Builder {
+        private Long id;
+        private String name;
+        private String username;
+        private String email;
+        private String hashPassword;
+
+        public Builder withId(Long id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder withName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder withUsername(String username) {
+            this.username = username;
+            return this;
+        }
+
+        public Builder withEmail(String email) {
+            this.email = email;
+            return this;
+        }
+
+        public Builder withHashPassword(String hashPassword) {
+            this.hashPassword = hashPassword;
+            return this;
+        }
+
+        public User build() {
+            return new User(id, name, username, email, hashPassword);
+        }
+    }
+
+
+}
