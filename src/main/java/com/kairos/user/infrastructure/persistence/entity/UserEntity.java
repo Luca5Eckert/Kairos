59d3942ae868103799b45d1d4b5138550d3b5589
luca5eckert <lucasismaeleckert@gmail.com>
2026-04-24 18:37:52 -0300

    #31 feat: add UserEntity class for user persistence

---
+++ src/main/java/com/kairos/user/infrastructure/persistence/entity/UserEntity.java
@@ -0,0 +1,32 @@
+package com.kairos.user.infrastructure.persistence.entity;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "password")
+    private String hashPassword;
+
+}
