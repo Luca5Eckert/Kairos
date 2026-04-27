e0ba361429870708eb570d7b252c596f8d869f8c
luca5eckert <lucasismaeleckert@gmail.com>
2026-04-27 14:24:14 -0300

    #33 feat: add RegisterRequest record for user registration

--- src/main/java/com/kairos/auth/presentation/dto/login/LoginRequest.java
+++ src/main/java/com/kairos/auth/presentation/dto/login/LoginRequest.java
@@ -1,12 +1,14 @@
 package com.kairos.auth.presentation.dto.login;
 
+import jakarta.validation.constraints.NotBlank;
+
 /**
  * LoginRequest is a record that represents the data required for a login request.
  * @param identifier The identifier can be either a username or an email address.
  * @param password The password associated with the identifier.
  */
 public record LoginRequest(
-        String identifier,
-        String password
+        @NotBlank String identifier,
+        @NotBlank String password
 ) {
 }
