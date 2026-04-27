59f572012a6267bd64e59f750c2907322e224d49
luca5eckert <lucasismaeleckert@gmail.com>
2026-04-27 14:24:34 -0300

    #33 feat: add register endpoint in AuthController

--- src/main/java/com/kairos/auth/presentation/controller/AuthController.java
+++ src/main/java/com/kairos/auth/presentation/controller/AuthController.java
@@ -1,15 +1,19 @@
 package com.kairos.auth.presentation.controller;
 
 import com.kairos.auth.application.command.LoginCommand;
+import com.kairos.auth.application.command.RegisterCommand;
 import com.kairos.auth.application.use_case.ConfirmEmailUseCase;
 import com.kairos.auth.application.use_case.LoginUseCase;
 import com.kairos.auth.application.use_case.RegisterUseCase;
 
 import com.kairos.auth.presentation.dto.login.LoginRequest;
 import com.kairos.auth.presentation.dto.login.LoginResponse;
+import com.kairos.auth.presentation.dto.register.RegisterRequest;
 
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,13 +32,27 @@ public class AuthController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<LoginResponse> login(LoginRequest request) {
+    public ResponseEntity<LoginResponse> login(@RequestBody @Valid LoginRequest request) {
         var command = LoginCommand.create(request.identifier(), request.password());
 
         var session = loginUseCase.execute(command);
         var response = LoginResponse.create(session.accessToken(), session.roles());
 
         return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<Void> register(@RequestBody @Valid RegisterRequest request){
+        var command = RegisterCommand.create(
+                request.name(),
+                request.username(),
+                request.email(),
+                request.password()
+        );
+
+        registerUseCase.execute(command);
+
+        return ResponseEntity.ok().build();
     }
 
 }
