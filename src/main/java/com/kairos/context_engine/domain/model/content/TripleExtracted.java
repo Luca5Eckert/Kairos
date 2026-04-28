f21182dfc8b52b5b97f0bedc0b6939d402277462
Luca5Eckert <ecklukatv@gmail.com>
2026-04-28 12:16:51 -0300

    #42 feat: add embedding functionality to TripleExtracted

--- src/main/java/com/kairos/context_engine/domain/model/content/TripleExtracted.java
+++ src/main/java/com/kairos/context_engine/domain/model/content/TripleExtracted.java
@@ -4,15 +4,17 @@ public class TripleExtracted {
 
     private final String key;
 
-    private final String suject;
+    private final String subject;
     private final String predicate;
     private final String object;
 
+    private float[] embedding;
+
     private final Chunk chunk;
 
-    protected TripleExtracted(String key, String suject, String predicate, String object, Chunk chunk) {
+    protected TripleExtracted(String key, String subject, String predicate, String object, Chunk chunk) {
         this.key = key;
-        this.suject = suject;
+        this.subject = subject;
         this.predicate = predicate;
         this.object = object;
         this.chunk = chunk;
@@ -23,12 +25,23 @@ public class TripleExtracted {
         return new TripleExtracted(key, suject, predicate, object, chunk);
     }
 
+    public void addEmbedding(float[] embedding) {
+        if (embedding == null || embedding.length == 0) {
+            throw new IllegalArgumentException("Embedding cannot be null or empty");
+        }
+        this.embedding = embedding;
+    }
+
+    public float[] getEmbedding() {
+        return embedding;
+    }
+
     public String getKey() {
         return key;
     }
 
     public String getSuject() {
-        return suject;
+        return subject;
     }
 
     public String getPredicate() {
