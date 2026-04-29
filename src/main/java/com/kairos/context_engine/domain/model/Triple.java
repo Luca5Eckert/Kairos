a8afe120deee9186c53549fbbed666c3cf15e086
luca5eckert <lucasismaeleckert@gmail.com>
2026-04-29 13:53:52 -0300

    #44 feat: add weight attribute to Triple

--- src/main/java/com/kairos/context_engine/domain/model/Triple.java
+++ src/main/java/com/kairos/context_engine/domain/model/Triple.java
@@ -3,6 +3,7 @@ package com.kairos.context_engine.domain.model;
 public record Triple(
         String subject,
         String predicate,
-        String object
+        String object,
+        double weight
 ) {
 }
