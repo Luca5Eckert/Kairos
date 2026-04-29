3b1a504ee3c758046907a8a830e0d15e765d4a2c
luca5eckert <lucasismaeleckert@gmail.com>
2026-04-29 16:54:45 -0300

    #44 feat: replace chunkId with Passage in KnowledgeTriple creation

--- src/main/java/com/kairos/context_engine/domain/model/knowledge/KnowledgeTriple.java
+++ src/main/java/com/kairos/context_engine/domain/model/knowledge/KnowledgeTriple.java
@@ -2,31 +2,29 @@ package com.kairos.context_engine.domain.model.knowledge;
 
 import com.kairos.context_engine.domain.model.Triple;
 
-import java.util.UUID;
-
 public record KnowledgeTriple(
         Concept subject,
         String predicate,
         Concept object,
-        UUID chunkId,
+        Passage passage,
         double weight
 ) {
-    public static KnowledgeTriple create(Triple triple, UUID chunkId) {
+    public static KnowledgeTriple create(Triple triple, Passage passage) {
         return new KnowledgeTriple(
                 Concept.create(triple.subject()),
                 triple.predicate(),
                 Concept.create(triple.object()),
-                chunkId,
+                passage,
                 triple.weight()
         );
     }
 
-    public static KnowledgeTriple create(String subject, String predicate, String object, UUID chunkId, double weight) {
+    public static KnowledgeTriple create(String subject, String predicate, String object, Passage passage, double weight) {
         return new KnowledgeTriple(
                 Concept.create(subject),
                 predicate,
                 Concept.create(object),
-                chunkId,
+                passage,
                 weight
         );
     }
