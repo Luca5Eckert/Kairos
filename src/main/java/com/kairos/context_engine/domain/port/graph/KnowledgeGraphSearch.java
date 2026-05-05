67d72bcd1ed868c2f6ef99b694b7f687ea287e65
luca5eckert <lucasismaeleckert@gmail.com>
2026-05-05 18:08:05 -0300

    #39 feat: add expandKnowledge method to KnowledgeGraphSearch interface for GraphSearchRequest

--- src/main/java/com/kairos/context_engine/domain/port/graph/KnowledgeGraphSearch.java
+++ src/main/java/com/kairos/context_engine/domain/port/graph/KnowledgeGraphSearch.java
@@ -2,9 +2,13 @@ package com.kairos.context_engine.domain.port.graph;
 
 import com.kairos.context_engine.domain.model.knowledge.KnowledgeTriple;
 import com.kairos.context_engine.domain.model.retrieval.candidate.PassageCandidate;
+import com.kairos.context_engine.domain.model.retrieval.graph.GraphSearchRequest;
+import com.kairos.context_engine.domain.model.retrieval.graph.GraphSearchResult;
 
 import java.util.List;
 
 public interface KnowledgeGraphSearch {
     List<KnowledgeTriple> expandKnowledge(List<PassageCandidate> semanticAnchors);
+
+    GraphSearchResult expandKnowledge(GraphSearchRequest graphSearchRequest);
 }
