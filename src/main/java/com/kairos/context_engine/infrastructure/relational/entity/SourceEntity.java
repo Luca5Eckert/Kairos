0bfc1cab6d8a41fdaff99b812d1ae23ea62a10f9
luca5eckert <lucasismaeleckert@gmail.com>
2026-05-08 20:09:14 -0300

    #53 refactor: enhance SourceEntity to include chunkEntities relationship

--- src/main/java/com/kairos/context_engine/infrastructure/relational/entity/SourceEntity.java
+++ src/main/java/com/kairos/context_engine/infrastructure/relational/entity/SourceEntity.java
@@ -1,15 +1,13 @@
 package com.kairos.context_engine.infrastructure.relational.entity;
 
 import com.kairos.context_engine.domain.model.content.Source;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.List;
 import java.util.UUID;
 
 @Entity
@@ -30,15 +28,26 @@ public class SourceEntity {
     @Column(nullable = false, columnDefinition = "TEXT")
     private String content;
 
+    @OneToMany(mappedBy = "source", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<ChunkEntity> chunkEntities;
+
     public SourceEntity(UUID id) {
         this.id = id;
+    }
+
+    public SourceEntity(UUID sourceId, String title, String content) {
+        this.id = sourceId;
+        this.title = title;
+        this.content = content;
+        this.chunkEntities = List.of();
     }
 
     public static SourceEntity of(Source source) {
         return new SourceEntity(
                 source.getId(),
                 source.getTitle(),
-                source.getContent()
+                source.getContent(),
+                List.of()
         );
     }
 
